bedad82cd96b78fe6fc9c8cd4b3df31ad46ab812
Katherine Milagros Quispe Pantoja <katherinebemol17@gmail.com>
2023-02-16 15:54:02 -0500

    Cuarta prueba

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
+
+/* module.exports = {
+  webpack: (config) => {
+    config.entry = ["@babel/polyfill", "./src/index.js"];
+    return config;
+  },
+};
+ */
